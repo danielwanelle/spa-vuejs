8dc49bd00bdd5d53a66486db4c86648bf8b854d6
danielwanelle <danielwanelle@gmail.com>
2021-05-26 00:35:26 -0300

    Add API de fotos e implementado o retorno

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -14,17 +14,13 @@ export default {
     data() {
         return {
             titulo: "Hello World",
-            fotos: [
-                {
-                    url: 'https://www.petz.com.br/blog/wp-content/uploads/2021/03/piercing-para-cachorro-2.jpg',
-                    alt: 'Cachorro'
-                },
-                {
-                    url: 'https://super.abril.com.br/wp-content/uploads/2018/05/filhotes-de-cachorro-alcanc3a7am-o-c3a1pice-de-fofura-com-8-semanas1.png',
-                    alt: 'Cachorrinho'
-                }
-            ]
+            fotos: []
         }
+    },
+    created() {
+        this.$http.get('http://localhost:3000/v1/fotos')
+            .then(res => res.json())
+            .then(fotos => this.fotos = fotos, err => console.log(err));
     }
 }
 </script>
